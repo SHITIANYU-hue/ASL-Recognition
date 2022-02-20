2e3417ab5497104996bd8f079b7cbac26acb4a8e
kellyzoo <kellyk.zhu@mail.utoronto.ca>
2022-02-20 10:13:47 -0500

    Add template for data processing file

---
+++ data_processing.py
@@ -0,0 +1,13 @@
+import torch
+
+def process_sign_language_MNIST_dataset():
+    pass
+
+def process_massey_gesture_dataset():
+    pass
+
+def process_fingerspelling_A_datatset():
+    pass
+
+if __name__ == "__main__":
+    pass
