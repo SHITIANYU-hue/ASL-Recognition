066bd57b9a8c897dc89475c8bdfaa44640500c13
kina <kina19131@gmail.com>
2022-02-26 11:18:25 -0500

    all works on kina's comp

--- data_processing.py
+++ data_processing.py
@@ -31,39 +31,19 @@ def process_sign_language_MNIST_dataset(filename):
         resizedImg = cv2.resize(img, (50,50), interpolation = cv2.INTER_CUBIC)
         resizedData.append(resizedImg)
 
-    # Plot first 24 resized images
-    col, ax = plt.subplots(4,6) 
-    col.set_size_inches(10, 10)
-    idx = 0
-    for i in range(4):
-        for j in range(6):
-            ax[i,j].imshow(resizedData[idx] , cmap = "gray")
-            idx += 1
-        plt.tight_layout()   
-    plt.show()
-
     return resizedData, data_labels
 
 def process_massey_gesture_dataset(path_to_files):
-    #files = ["/Users/kinakim/Desktop/massey/1", "/Users/kinakim/Desktop/massey/2", "/Users/kinakim/Desktop/massey/3", "/Users/kinakim/Desktop/massey/4", "/Users/kinakim/Desktop/massey/5"]
     ret_data = [] 
     ret_label = []
 
-    # dirs = os.listdir(path_to_files)
-
-    # for list_of_files in os.listdir(path_to_files): 
-    #     filepath = path_to_files+'/'+list_of_files
-    #     if os.path.isdir(filepath):
-    #         os.chdir(filepath)
     for subdir, dirs, files in os.walk(path_to_files):
         for file in files:
             if "DS_Store" in file:
                 continue
-
             image = os.path.join(subdir, file)
-            img = cv2.imread(image)
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            resized = cv2.resize(gray, (50,50))
+            img = cv2.imread(image, cv2.IMREAD_GRAYSCALE)
+            resized = cv2.resize(img, (50,50))
             ret_data.append(resized)
             ret_label.append(image)
     
@@ -125,10 +105,11 @@ if __name__ == "__main__":
     data_mnist, labels_mnist = process_sign_language_MNIST_dataset("datasets/mnist/sign_mnist_data.csv")
     print("Processing MNIST done!")
     
+    print("Processing Massey...")
+    data_massey, labels_massey = process_massey_gesture_dataset("datasets/massey") # path to massey directory. massey directory has subdir 1,2,3,4,5 
+    print("Processing Massey done!")
+
     print("Processing FingerSpelling...")
     data_fs, labels_fs = process_fingerspelling_A_dataset("datasets/fingerspelling")
     print("Processing FingerSpelling done!")
 
-    print("Processing Massey...")
-    data_massey, labels_massey = process_massey_gesture_dataset("datasets/massey") # path to massey directory. massey directory has subdir 1,2,3,4,5 
-    print("Processing Massey done!")
