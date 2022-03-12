4880800351807a832d969aad4ef05d1cb3b0ee7f
kellyzoo <kellyk.zhu@mail.utoronto.ca>
2022-03-12 12:33:01 -0500

    Initial implementation of minibatch SGD

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-from data_processing import load_data_from_pickle
+from data_processing import load_data_from_pickle, one_hot_encoding, process_massey_gesture_dataset
 import torchvision.models as models
 import torch
 # from torchsummary import summary 
@@ -8,43 +8,19 @@ def set_requires_grad_false(model):
     for param in model.parameters():
         param.requires_grad = False
 
-if __name__ == "__main__":
-    # Load Data
-    print("Loading dataset...")
-    data = load_data_from_pickle("data.pkl")
-    X_train = data["X_train"]
-    y_train = data["y_train"]
-    X_valid = data["X_valid"]
-    y_valid = data["y_valid"]
-    X_test = data["X_test"]
-    y_test = data["y_test"]
-
-    # Change input data to conform to CNN input shape
-    X_train = torch.permute(X_train, (0, 3, 1, 2))
-
-    # Convert to floating point numbers
-    y_train = y_train.to(dtype=torch.float32)
-
+def train(model, X, y):
     # Variables
-    num_classes = 24
-    lr = 0.001
-    num_epochs = 10
-    
-    # Create GoogLeNet model
-    print("Loading GoogLeNet model...")
-    model = models.googlenet(pretrained=True)
-    print(model) 
-    
-    # Change last output layer to match output dimension
-    set_requires_grad_false(model)
-    model.fc = torch.nn.Linear(1024, num_classes)
+    LEARNING_RATE = 0.001
+    EPOCHS = 10
+    BATCH_SIZE = 256
+    BATCHES = int(len(X) / BATCH_SIZE) 
 
     # Set optimizer
     params_to_update = []
     for param in model.parameters():
         if param.requires_grad == True:
             params_to_update.append(param)
-    optimizer = torch.optim.SGD(params_to_update, lr=lr)
+    optimizer = torch.optim.SGD(params_to_update, lr=LEARNING_RATE)
 
     # Loss Function
     loss = torch.nn.CrossEntropyLoss()
@@ -58,20 +34,52 @@ if __name__ == "__main__":
 
     # Training loop
     print("Starting training loop...")
-    for epoch in range(num_epochs):
-        optimizer.zero_grad()
+    for epoch in range(EPOCHS):
+        avg_train_loss = 0
+        for batch in range(BATCHES):
+            X_batch = X[batch * BATCH_SIZE:(batch + 1) * BATCH_SIZE,] 
+            y_batch = y[batch * BATCH_SIZE:(batch + 1) * BATCH_SIZE,]
+            
+            # Make prediction
+            train_pred = model(X_batch)
+            train_loss = loss(torch.squeeze(train_pred), y_batch)
+            avg_train_loss += train_loss * BATCH_SIZE
 
-        # Model predictions
-        train_pred = torch.squeeze(model(X_train))
+            optimizer.zero_grad()
+            train_loss.backward()
+            optimizer.step()
+        
+        train_losses.append(avg_train_loss/X_train.shape[0])
+        # train_losses.append(100.0*train_loss.detach().numpy())
+        print('Epoch %04d  Training Loss %.4f' % (epoch + 1, train_loss))   
 
-        # Get loss
-        train_loss = loss(train_pred, y_train)
+if __name__ == "__main__":
+    # Load Data
+    print("Loading dataset...")
+    data = load_data_from_pickle("data.pkl")
+    X_train = data["X_train"]
+    y_train = data["y_train"]
+    X_valid = data["X_valid"]
+    y_valid = data["y_valid"]
+    X_test = data["X_test"]
+    y_test = data["y_test"]
+    print("Data loaded!")
 
-        train_loss.backward()
-        optimizer.step()
+    # Change input data to conform to CNN input shape
+    X_train = torch.permute(X_train, (0, 3, 1, 2))
 
-        # Append loss
-        train_losses.append(100.0*train_loss.detach().numpy())
+    # Convert to floating point numbers
+    y_train = y_train.to(dtype=torch.float32)
 
-        print('Epoch %04d  Training Loss %.4f' % (epoch + 1, train_loss))
+    # Create GoogLeNet model
+    print("Loading GoogLeNet model...")
+    model = models.googlenet(pretrained=True)
+    print("Model loaded!")
+    
+    # Change last output layer to match output dimension
+    num_classes = 24
+    set_requires_grad_false(model)
+    model.fc = torch.nn.Linear(1024, num_classes)
+
+    train(model, X_train, y_train)
 
